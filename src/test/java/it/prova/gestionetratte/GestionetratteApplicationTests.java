8148a6e14c8a0920d0a8a16d0730b4694cbad255
giuliacimino <giuliacimino00@gmail.com>
2023-05-19 12:05:03 +0200

    inizio progetto

---
+++ src/test/java/it/prova/gestionetratte/GestionetratteApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.gestionetratte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionetratteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
